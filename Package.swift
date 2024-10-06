597f61c46f72bea7cc10095445e4ddfcc9771b78
Jonathan Gilat <jgilat3@gmail.com>
2024-10-06 12:11:48 +0300

    set deployment to ios 16, for real

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "MedidaSwiftUtils",
     defaultLocalization: "en",
     platforms: [
-        .iOS(.v17)
+        .iOS(.v16)
     ],
     products: [
         .library(
